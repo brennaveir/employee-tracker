f6fa18504826f12ec6c5a933cd6fd0c5e820d2dc
brennaveir <brennaveir@hotmail.com>
2023-02-21 14:52:03 -0500

    added query to select manager name instead of id using left outer join

--- db/query.sql
+++ db/query.sql
@@ -4,6 +4,7 @@ JOIN departments
 ON roles.department_id = departments.id
 ORDER BY departments.department_name;
 
+
 SELECT 
 employees.id,
 employees.first_name AS First,
@@ -11,17 +12,21 @@ employees.last_name AS Last,
 roles.title AS Title,
 departments.department_name AS Department, 
 roles.salary AS Salary,
-employees.manager_id AS Manager
+CONCAT(m.first_name, " ", m.last_name) AS Manager
 FROM departments
 JOIN roles 
 ON departments.id = roles.department_id
 JOIN employees 
 ON roles.id = employees.role_id
+LEFT OUTER JOIN employees m
+ON m.ID = employees.manager_id 
 ORDER BY employees.id;
+
 
 SELECT e.ID, 
 e.first_name AS First, 
 e.last_name AS Last,
 CONCAT(m.first_name, " ", m.last_name) AS Manager
-FROM employees e INNER JOIN employees m
+FROM employees e 
+LEFT OUTER JOIN employees m
 ON m.ID = e.manager_id ORDER BY e.ID;
