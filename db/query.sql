89b92ce9a9525b1b8b9d8a13b89a7ba95fecb6d5
brennaveir <brennaveir@hotmail.com>
2023-02-21 14:19:09 -0500

    added queries to join all three table together in query.sql

--- db/query.sql
+++ db/query.sql
@@ -1,5 +1,20 @@
 SELECT departments.department_name AS department, roles.title
 FROM roles
-LEFT JOIN departments
+JOIN departments
 ON roles.department_id = departments.id
 ORDER BY departments.department_name;
+
+SELECT 
+employees.id,
+employees.first_name AS First,
+employees.last_name AS Last,
+roles.title AS Title,
+departments.department_name AS Department, 
+roles.salary AS Salary,
+employees.manager_id AS Manager
+FROM departments
+JOIN roles 
+ON departments.id = roles.department_id
+JOIN employees 
+ON roles.id = employees.role_id;
+
--- db/schema.sql
+++ db/schema.sql
@@ -23,7 +23,7 @@ CREATE TABLE employees (
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
-    manager_id INT REFERENCES employees,
+    manager_id INT REFERENCES employees(id),
     FOREIGN KEY (role_id)
     REFERENCES roles(id)
     ON DELETE SET NULL
