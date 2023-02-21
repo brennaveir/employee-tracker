5c0f9f421c204a3d91b11c1e85ad716c08749afe
brennaveir <brennaveir@hotmail.com>
2023-02-21 14:30:28 -0500

    added query to show manager name with employee

--- db/query.sql
+++ db/query.sql
@@ -16,5 +16,12 @@ FROM departments
 JOIN roles 
 ON departments.id = roles.department_id
 JOIN employees 
-ON roles.id = employees.role_id;
+ON roles.id = employees.role_id
+ORDER BY employees.id;
 
+SELECT e.ID, 
+e.first_name AS First, 
+e.last_name AS Last,
+CONCAT(m.first_name, " ", m.last_name) AS Manager
+FROM employees e INNER JOIN employees m
+ON m.ID = e.manager_id ORDER BY e.ID;
