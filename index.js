2c9c21d705810a460b4113dc4d8df3435a7d99b7
brennaveir <brennaveir@hotmail.com>
2023-02-21 15:49:18 -0500

    added inquirer prompts for a user directory

--- index.js
+++ index.js
@@ -1,2 +1,37 @@
-const inquirer = require('inquirer')
+const inquirer = require('inquirer');
 const fs = require('fs/promises');
+const mysql = require('mysql2');
+const cTable = require('console.table');
+
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // MySQL username,
+      user: 'root',
+      // TODO: Add MySQL password
+      password: 'H#raB3ra!',
+      database: 'tracker_db'
+    },
+    console.log(`Connected to the books_db database.`)
+  );
+
+function run() {
+    return inquirer
+        .prompt([
+            {
+//WHEN I start the application THEN I am presented with the following options: 
+//view all departments, view all roles, view all employees, add a department, add a role, 
+//add an employee, and update an employee role
+                type: 'list',
+                name: 'action',
+                message: 'What would you like to do?',
+                choices: [
+                    'View all departments', 'View all roles',
+                    'View all employees', 'Add a department', 'Add a role',
+                    'Add an employee', 'Update an employee role'
+                ]
+            },
+        ])
+}
+
+run()
